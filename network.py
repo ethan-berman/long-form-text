a22659b16d6f9bce4b4e4d1688cdd0f89dc2cbe2
eberman99@gmail.com <ethanberman@ethans-mbp.milton.edu>
2017-10-26 12:19:15 -0400

    first commit

--- network.py
+++ network.py
@@ -11,6 +11,7 @@ pruned = []
 limit = int(training["text"].size)
 
 def clean_tweet(tweet):
+    #improve me, i make ugly text and I get rid ofthe wrong characters
     letters = re.sub("[^a-zA-Z]", " ", tweet)
     words = letters.lower().split()
     stops = set(stopwords.words("english"))
@@ -21,12 +22,16 @@ def clean_tweet(tweet):
 for i in range(0, limit):
     if training["is_retweet"][i] == False:
         cleaned = clean_tweet(training["text"][i])
+        #cleaned should be vectorized before appending to pruned
         user = training["handle"][i]
         retweets = training["retweet_count"][i]
         favorites = training["favorite_count"][i]
         pruned.append({user : {(retweets, favorites) : cleaned}})
 
 print(pruned)
+#the above code can be used for all three approaches, although edits should be made to the clean_tweet function
+#Next Step for bag of words is to Vectorize texts before going into pruned
+
 #clean tweets add the text to a dictionary corresponding to which person tweeted it
 #backwards propogation
 
